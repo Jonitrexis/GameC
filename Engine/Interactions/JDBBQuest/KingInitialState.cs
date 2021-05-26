0790895b3255c438aa033149a9ee8212cfedf0b9
Jonitrexis <jonatanlski2@gmail.com>
2021-05-26 16:42:19 +0200

    Dodany KingInitialState, KingEncounter i KingState

---
+++ Engine/Interactions/JDBBQuest/KingInitialState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Engine.Interactions.JDBBQuest
+{
+    class KingInitialState : KingState
+    {
+        public override void RunContent(GameSession ses, KingEncounter myself)
+        {
+            ses.SendText("\n*strażnicy zagradzają Ci drogę do komnaty Króla*");
+            int choice = ses.GetListBoxChoice(new List<string>() { "*Odejdź od króla*"});
+            if (choice == 0)
+            {
+                ses.SendText("\n*Słyszysz zatrzaskujące się za Tobą drzwi*");
+            }
+        }
+    }
+}
