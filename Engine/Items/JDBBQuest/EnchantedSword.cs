941c97677c37adba6a72aa49684d09cbbe63226a
Jonitrexis <jonatanlski2@gmail.com>
2021-05-26 16:17:19 +0200

    Dodany enchantowany miecz

---
+++ Engine/Items/JDBBQuest/EnchantedSword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Engine.Items.JDBBQuest
+{
+    class EnchantedSword: Sword
+    {
+        public EnchantedSword() : base("item9001")
+        {
+            StrMod = 50;
+            GoldValue = 100;
+            PublicName = "Enczantowany Miecz";
+        }
+    }
+}
