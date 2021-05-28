708c09eeb593ea4b4a7f247eec7f9b46e4c70b2b
Jonitrexis <jonatanlski2@gmail.com>
2021-05-28 17:23:25 +0200

    secretary complete,in progress true/false

---
+++ Engine/Interactions/JDBBQuest/SecretaryCompleteState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Engine.Interactions.JDBBQuest
+{
+    [Serializable]
+    class SecretaryCompleteState : SecretaryState
+    {
+        public override void RunContent(GameSession ses, SecretaryEncounter myself, AntyPopeEncounter antyPope, PopeEncounter pope)
+        {
+            ses.SendText("\n*Sekretarz podnosi wzrok znad dokumentów i nic nie mówiąc wraca do pracy.*");
+        }
+    }
+}
---
+++ Engine/Interactions/JDBBQuest/SecretaryInProgressTrueState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Engine.Interactions.JDBBQuest
+{
+    [Serializable]
+    class SecretaryInProgressTrueState:SecretaryState
+    {
+        public override void RunContent(GameSession ses, SecretaryEncounter myself, AntyPopeEncounter antyPope, PopeEncounter pope)
+        {
+            ses.SendText("\nWitaj wędrowcze, dzięki Ci za rozprawienie się z potworami." +
+                " Miałbym dla Ciebie jeszcze jedno zlecenie. Potrzebuję by ktoś odpowiedzialny" +
+                " przekazał ten oto FRAGMENT EDENU Papieżowi. Pod żadnym pozorem nie zanoś go AntyPapieżowi!");
+            antyPope.currentStrategy = new AntyPopeFalseStrategy();
+            pope.currentStrategy = new PopeFalseStrategy();
+            myself.ChangeState(new SecretaryCompleteState(), true);
+        }
+    }
+}
