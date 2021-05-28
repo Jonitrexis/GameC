3970bef5f9a1b3278f969648c92f4c12e95c650e
Jonitrexis <jonatanlski2@gmail.com>
2021-05-28 19:07:46 +0200

    eden fragment

---
+++ Engine/Items/JDBBQuest/EdenFragment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Engine.Items.JDBBQuest
+{
+    class EdenFragment:JDBBQuestItem
+    {
+        EdenFragment() : base("item9005")
+        {
+            PublicName = "Fragment Edenu";
+            GoldValue = 0;
+            StrMod = 0;
+            PublicTip = "Podczas wykonywania questa miej ten przedmiot wyekwipowany.";
+        }
+    }
+}
---
+++ Engine/Items/JDBBQuest/JDBBQuestItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Engine.Items.JDBBQuest
+{
+    [Serializable]
+    abstract class JDBBQuestItem : Item
+    {
+        public JDBBQuestItem(string name) : base(name)
+        {   
+        }
+    }
+}
